655273f0c3a3a42936bd0e117c1632a3d0b3b9e7
Kaz_Macintosh <52682342+kaz-mac@users.noreply.github.com>
2022-01-23 08:56:40 +0900

    Add files via upload

---
+++ VRChat/datacenter/Humidai_OnOff.cs
@@ -0,0 +1,19 @@
+﻿//
+// オブジェクトをON/OFFする
+//
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Humidai_OnOff : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject[] _targets;
+
+    public override void Interact()
+    {
+        for (int i = 0; i < _targets.Length; i++) { 
+            _targets[i].SetActive(!_targets[i].activeSelf);
+        }
+    }
+}
