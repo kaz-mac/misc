e5729a3fb976df2bdd383e8dd6713c7b9742b728
Kaz_Macintosh <52682342+kaz-mac@users.noreply.github.com>
2022-01-22 17:18:54 +0900

    Add files via upload

---
+++ VRChat/datacenter/Pickup_Use_Toggle_with_Sound.cs
@@ -0,0 +1,34 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Pickup_Use_Toggle_with_Sound : UdonSharpBehaviour
+{
+    AudioSource thisAudioSource;
+    [SerializeField] GameObject[] objs;
+
+    void Start()
+    {
+        thisAudioSource = this.GetComponent<AudioSource>();
+    }
+
+    public override void OnPickupUseDown()
+    {
+        if (!Networking.IsOwner(Networking.LocalPlayer, this.gameObject)) Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "ToggleObjs_and_PlaySound");
+    }
+
+    public void ToggleObjs_and_PlaySound()
+    {
+        if (objs.Length > 0)
+        {
+            if (objs[0].activeSelf) thisAudioSource.Stop();
+            else thisAudioSource.Play();
+        }
+        for (var i = 0; objs.Length > i; i++)
+        {
+            objs[i].SetActive(!objs[i].activeSelf);
+        }
+    }
+}
